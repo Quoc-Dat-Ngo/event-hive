b3b9f49f276e3af933aa9592023d8b4434d32ea7
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-07-08 21:18:06 +1000

    Initialised new Spring Boot project with required dependencies

---
+++ backend/src/test/java/com/eventhive/EventhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
