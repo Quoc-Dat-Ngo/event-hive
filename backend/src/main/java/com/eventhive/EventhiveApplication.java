727c7f5a31bb2e88a4789b1eb55e325e91bbc48e
Kevin Ngo <quocdatngo2003@gmail.com>
2026-07-12 21:24:43 +1000

    Disable Spring Security default login page

--- backend/src/main/java/com/eventhive/EventhiveApplication.java
+++ backend/src/main/java/com/eventhive/EventhiveApplication.java
@@ -1,7 +1,13 @@
 package com.eventhive;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.eventhive.users.User;
 
 @SpringBootApplication
 public class EventhiveApplication {
@@ -9,5 +15,23 @@ public class EventhiveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventhiveApplication.class, args);
 	}
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.csrf((csrf) -> csrf.disable());
+		return http.build();
+	}
+
+	// @Bean
+	// CommandLineRunner runner() {
+	// return args -> {
+	// createRandomUser();
+	// };
+	// }
+
+	// private static void createRandomUser() {
+	// User user = new User
+	// }
 
 }
