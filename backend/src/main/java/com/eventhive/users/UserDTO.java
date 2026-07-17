9f13eef6cf6418a0f02c5fa3c69a9d8a0c1d3985
Kevin Ngo <quocdatngo2003@gmail.com>
2026-07-17 15:54:44 +1000

    Constructed request payload using Java record

--- backend/src/main/java/com/eventhive/users/UserDTO.java
+++ backend/src/main/java/com/eventhive/users/UserDTO.java
@@ -4,12 +4,12 @@ import java.time.Instant;
 import java.util.UUID;
 
 public record UserDTO(
-        UUID id,
-        String firstName,
-        String lastName,
-        String email,
-        String provider,
-        String role,
-        Instant createdAt,
-        Instant updatedAt) {
+		UUID id,
+		String firstName,
+		String lastName,
+		String email,
+		String provider,
+		String role,
+		Instant createdAt,
+		Instant updatedAt) {
 }
