0f0fca74ddb2c70b890bbcd31466b780d7b12fad
Kevin Ngo <quocdatngo2003@gmail.com>
2026-07-17 15:51:28 +1000

    Created a Bean for PasswordEnconder using BCrypt

---
+++ backend/src/main/java/com/eventhive/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.eventhive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
