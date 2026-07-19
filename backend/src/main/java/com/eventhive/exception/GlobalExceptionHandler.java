1a291a3cb334ffdad420e31eb22b1eecbb32a852
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-07-19 20:53:14 +1000

    Add a catch-all exception, DataIntegrityViolationException exception and MethodArgumentNotValidException exception handler

--- backend/src/main/java/com/eventhive/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/eventhive/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.eventhive.exception;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -32,7 +35,6 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now());
 
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
-
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
@@ -44,6 +46,42 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now());
 
         return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
-
     }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> handleException(DataIntegrityViolationException e, HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                "A data conflict occured",
+                HttpStatus.CONFLICT.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                message,
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception e, HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                "An unexpected error occurred",
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now());
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
