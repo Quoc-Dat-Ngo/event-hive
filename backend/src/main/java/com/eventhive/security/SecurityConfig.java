0e6321ac2fea1b81ca1e060a426fbb4db98a33d9
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-08-06 12:43:23 +1000

    Move temporary permitAll() filterChain to SecurityConfig file to adhere to the principle separate of concern

--- backend/src/main/java/com/eventhive/security/SecurityConfig.java
+++ backend/src/main/java/com/eventhive/security/SecurityConfig.java
@@ -2,16 +2,25 @@ package com.eventhive.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-import lombok.RequiredArgsConstructor;
 
 @Configuration
-@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.csrf((csrf) -> csrf.disable());
+		return http.build();
+	}
 }
