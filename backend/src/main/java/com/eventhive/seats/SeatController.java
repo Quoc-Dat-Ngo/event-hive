05d54a7518b4aab424eac4c8d41ce24a32be457d
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-08-08 16:56:51 +1000

    Add return HTTP status code for adding method as 201 CREATED

--- backend/src/main/java/com/eventhive/seats/SeatController.java
+++ backend/src/main/java/com/eventhive/seats/SeatController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eventhive.events.VenueSummaryDTO;
@@ -49,6 +51,7 @@ public class SeatController {
     }
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public SeatDTO addNewSeat(@Valid @RequestBody SeatRegistrationRequest request) {
         return service.addSeat(request);
     }
