927cbdbfa5c8a3f7d01430fc7b6b36e42b855cd5
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-08-11 14:14:20 +1000

    Create beans for the family of spring security classes

--- backend/src/main/java/com/eventhive/security/SecurityConfig.java
+++ backend/src/main/java/com/eventhive/security/SecurityConfig.java
@@ -1,26 +1,77 @@
 package com.eventhive.security;
 
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
 				.csrf((csrf) -> csrf.disable());
 		return http.build();
+	}
+
+	@Bean
+	SecretKey secretKey() {
+		return new SecretKeySpec(jwtSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+	}
+
+	@Bean
+	JwtEncoder jwtEncoder(SecretKey secretKey) {
+		return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey));
+	}
+
+	@Bean
+	JwtDecoder jwtDecoder(SecretKey secretKey) {
+		return NimbusJwtDecoder
+				.withSecretKey(secretKey)
+				.macAlgorithm(MacAlgorithm.HS256)
+				.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	AuthenticationProvider authenticationProvider(
+			PasswordEncoder passwordEncoder,
+			AppUserDetailsService appUserDetailsService) {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(appUserDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		return provider;
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+		return configuration.getAuthenticationManager();
 	}
 }
