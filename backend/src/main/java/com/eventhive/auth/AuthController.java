671392ccd5b5bad62e8d9bb2f599a91aca0e7695
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-08-14 11:39:30 +1000

    Include userId as a claim in JWT payload; modify return type for RegisterResponse

--- backend/src/main/java/com/eventhive/auth/AuthController.java
+++ backend/src/main/java/com/eventhive/auth/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eventhive.users.UserDTO;
 import com.eventhive.users.UserRegistrationRequest;
 import com.eventhive.users.UserService;
 
@@ -38,11 +39,11 @@ public class AuthController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public RegisterResponse register(
             @Valid @RequestBody UserRegistrationRequest request) {
-        userService.createStandardUser(request);
+        UserDTO newUser = userService.createStandardUser(request);
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                 request.email(), request.password());
         Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
         String token = jwtTokenService.generateToken(authentication);
-        return new RegisterResponse(token);
+        return new RegisterResponse(token, newUser);
     }
 }
