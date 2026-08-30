63a8f979296c74416ecd9577f9ae3973713dd7a3
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-08-31 01:23:32 +1000

    Seperate out subject, principalId and authorities instead of one authentication object

--- backend/src/main/java/com/eventhive/auth/JwtTokenService.java
+++ backend/src/main/java/com/eventhive/auth/JwtTokenService.java
@@ -2,8 +2,8 @@ package com.eventhive.auth;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collection;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwsHeader;
@@ -12,34 +12,38 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
-import com.eventhive.security.UserPrincipal;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class JwtTokenService {
-    private final JwtEncoder jwtEncoder;
+	private final JwtEncoder jwtEncoder;
 
-    String generateToken(Authentication authentication) {
-        Instant now = Instant.now();
-        var authorities = authentication.getAuthorities();
-        var roles = authorities.stream()
-                .map(GrantedAuthority::getAuthority)
-                .filter(r -> r.startsWith("ROLE_"))
-                .map(r -> r.substring("ROLE_".length()))
-                .toList();
+	String generateToken(
+			String subject,
+			String principalId,
+			Collection<? extends GrantedAuthority> authorities) {
+		Instant now = Instant.now();
+		// var authorities = authentication.getAuthorities();
+		var roles = authorities.stream()
+				.map(GrantedAuthority::getAuthority)
+				.filter(r -> r.startsWith("ROLE_"))
+				.map(r -> r.substring("ROLE_".length()))
+				.toList();
 
-        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-                .issuedAt(now)
-                .expiresAt(now.plus(15, ChronoUnit.MINUTES))
-                .subject(authentication.getName())
-                .claim("userId", ((UserPrincipal) authentication.getPrincipal()).getId().toString())
-                .claim("roles", roles)
-                .build();
-        JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
-        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(jwsHeader, jwtClaimsSet);
+		JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+				.issuedAt(now)
+				.expiresAt(now.plus(15, ChronoUnit.MINUTES))
+				// .subject(authentication.getName())
+				.subject(subject)
+				// .claim("userId", ((UserPrincipal)
+				// authentication.getPrincipal()).getId().toString())
+				.claim("userId", principalId)
+				.claim("roles", roles)
+				.build();
+		JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
+		JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(jwsHeader, jwtClaimsSet);
 
-        return jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
-    }
+		return jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+	}
 }
